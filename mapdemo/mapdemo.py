f736b68af314039d3427a1c4bb14115380ce6555
Atul Bhardwaj <atulmncfc@gmail.com>
2025-09-17 16:20:05 +0530

    add same example

--- mapdemo/mapdemo.py
+++ mapdemo/mapdemo.py
@@ -1,7 +1,7 @@
 """Main module."""
 
 import ipyleaflet
-from ipyleaflet import basemaps, TileLayer
+from ipyleaflet import basemaps, TileLayer, WidgetControl 
 import ipywidgets as widgets
 
 
@@ -230,7 +230,12 @@ class Map(ipyleaflet.Map):
         self.add(control)
 
     def add_basemap_gui(self,basemaps=None, position="topright"):
+        """_summary_
 
+        Args:
+            basemaps (_type_, optional): _description_. Defaults to None.
+            position (str, optional): _description_. Defaults to "topright".
+        """
         basemap_selector=widgets.Dropdown(
             options=[
                 "OpenStreetMap",
@@ -248,6 +253,92 @@ class Map(ipyleaflet.Map):
 
         control = ipyleaflet.WidgetControl(widget=basemap_selector, position=position)
         self.add(control)
+
+    def add_toolbar(self, position="topright"):
+        """Adds a toolbar to the map
+
+        Args:
+            position (str, optional): The position of the toolbar. Defaults to "topright".
+        """        
+        padding = "0px 0px 0px 5px"
+
+        toolbar_button = widgets.ToggleButton(
+            value=False,
+            tooltip="Toolbar",
+            icon="wrench",
+            layout=widgets.Layout(height='28px',width='28px', padding=padding),
+        )
+
+        close_button = widgets.ToggleButton(
+            value=False,
+            tooltip="Close the tool",
+            icon="times",
+            button_style='primary',
+            layout=widgets.Layout(height='28px', width='28px', padding=padding),
+        )
+
+        toolbar = widgets.VBox([toolbar_button])
+
+        def close_click(change):
+            if change['new']:
+                toolbar_button.close()
+                close_button.close()
+                toolbar.close()
+
+        close_button.observe(close_click,'value')
+
+        rows = 2
+        cols = 2
+        grid = widgets.GridspecLayout(
+            rows, cols, grid_gap="0px", layout=widgets.Layout(width='65px')
+        )
+
+        icon = ['folder-open','map','info','question']
+
+        for i in range(rows):
+            for j in range (cols):
+                grid[i,j] = widgets.Button(
+                    description="",
+                    button_style='primary',
+                    icon=icon[i* rows + j],
+                    layout=widgets.Layout(width='28px', padding='0px'),
+                )
+
+        def toolbar_click(change):
+            if change ['new']:
+                toolbar.children=[widgets.HBox([close_button,toolbar_button]),grid]
+            else:
+                toolbar.children = [toolbar_button]
+
+        toolbar_button.observe(toolbar_click,'value')
+
+        toolbar_ctrl = WidgetControl(widget=toolbar, position ="topright")
+        self.add(toolbar_ctrl)
+
+        output = widgets.Output()
+        output_control = WidgetControl(widget=output, position='bottomright')
+        self.add(output_control)
+
+        def toolbar_callback(change):
+            if change.icon == "folder-open":
+                with output:
+                    output.clear_output()
+                    print(f"you can open a file")
+            elif change.icon == 'map':
+                with output:
+                    output.clear_output()
+                    print(f"you can add a basemap")
+            else:
+                with output:
+                    output.clear_output()
+                    print(f"Icon: {change.icon}")        
+           
+            for tool in grid.children:
+                tool.on_click(toolbar_callback)
+  
+              
+
+
 
         
 
