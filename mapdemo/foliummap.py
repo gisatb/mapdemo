7a985e330a24c5c6c153c92f17ed5db6461a8209
Atul Bhardwaj <atulmncfc@gmail.com>
2025-09-18 17:21:14 +0530

    add new module

--- mapdemo/foliummap.py
+++ mapdemo/foliummap.py
@@ -1,4 +1,5 @@
 import folium
+from ipyleaflet import basemaps
 
 class Map(folium.Map):
 
@@ -25,3 +26,55 @@ class Map(folium.Map):
         client =TileClient(data)
         layer=get_folium_tile_layer(client, name=name, **kwargs)
         layer.add_to(self)
+
+    def add_tile_layer(self, url, name, attribution="custom tile",**kwargs):
+        """Add a tile layer to the map
+
+        Args:
+            url (_type_): url of the layer
+            name (_type_): _descrName of the layer added to the layeription_
+            attribution (str, optional): _description_. Defaults to "custom tile".
+        """        
+        layer = folium.TileLayer(tiles=url, name=name, attr=attribution, **kwargs)
+        layer.add_to(self)
+
+    def add_basemap(self, name, overlay=True,**kwargs):
+        """Adds a basemap to the current map
+
+        Args:
+            name (_type_): The name of the basemap as a string, or an object
+        """
+        if isinstance(name, str):
+            try:
+                provider = basemaps
+                for part in name.split("."):
+                    provider = getattr(provider, part)
+
+                url = provider.build_url()
+                attribution = provider.attribution
+
+                # Add the provider as a TileLayer
+                layer = folium.TileLayer(tiles=url, name=name,attr=attribution, **kwargs)
+                layer.add_to(self)
+            except Exception as e:
+                raise ValueError(f"Basemap '{name}' not found: {e}")        
+        else:
+            name.add_to(self)
+
+
+    def to_streamlit(self, width=700, height=500):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        from streamlit_folium import folium_static
+        return folium_static(self, width=width, height=height)
+    
+
+    def add_layer_control(self):
+        """Add a layer control to the map
+        
+        """
+        folium.LayerControl().add_to(self)
+             
